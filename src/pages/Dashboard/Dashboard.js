b6703c65ad4fec70ff44e2d61dd6b6eb2abeb096
brunogomes98 <brunog.vix@gmail.com>
2022-04-24 04:43:55 -0300

    finished the dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -8,30 +8,60 @@ import { useAuthValue } from "../../context/AuthContext";
 import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 
 const Dashboard = () => {
-  const {user} = useAuthValue()
-  const uid = user.uid
+  const { user } = useAuthValue();
+  const uid = user.uid;
 
   // posts from user
-  const {documents: posts, loading} = useFetchDocuments("posts", null, uid);
+  const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
+
+  const deleteDocument = (id) => {};
+
+  if(loading) {
+    return <p>Loading...</p>
+  }
 
   return (
-    <div>
+    <div className={styles.dashboard}>
       <h1>Dashboard </h1>
       <p>Manager your posts</p>
       {posts && posts.length === 0 ? (
         <div className={styles.noposts}>
           <p>Not found any posts</p>
-          <Link to="/posts/create" className="btn"  >Create your post</Link>
-          </div>
+          <Link to="/posts/create" className="btn">
+            Create your post
+          </Link>
+        </div>
       ) : (
-        <div>
-          <p>Your posts</p>
+        <>
+          <div className={styles.post_header}>
+            <span>Title</span>
+            <span>Actions</span>
           </div>
+          {posts &&
+            posts.map((post) => (
+              <div key={post.id} className={styles.post_row}>
+                <p>{post.title}</p>
+                <div>
+                  <Link to={`/posts/${post.id}`} className="btn btn-outline">
+                    Read
+                  </Link>
+                  <Link
+                    to={`/posts/edit/${post.id}`}
+                    className="btn btn-outline"
+                  >
+                    Edit
+                  </Link>
+                  <button
+                    onClick={() => deleteDocument(post.id)}
+                    className="btn btn-outline btn-danger"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            ))}
+        </>
       )}
-
-      {posts && posts.map((post) => (
-        <h3>{post.title}</h3>
-      ))}
     </div>
   );
 };
