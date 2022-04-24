634d532b2d8b71e1aa2691025fb09c771861c49c
brunogomes98 <brunog.vix@gmail.com>
2022-04-24 05:02:47 -0300

    deleting is working

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 
 import { useAuthValue } from "../../context/AuthContext";
 import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+import { useDeleteDocument } from "../../hooks/useDeleteDocument";
 
 const Dashboard = () => {
   const { user } = useAuthValue();
@@ -14,7 +15,8 @@ const Dashboard = () => {
   // posts from user
   const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
 
-  const deleteDocument = (id) => {};
+  const {deleteDocument} = useDeleteDocument("posts");
+
 
   if(loading) {
     return <p>Loading...</p>
