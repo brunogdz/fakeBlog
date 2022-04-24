3fe9627a20f9b44f95fca535d5726cbabb2cdff9
brunogomes98 <brunog.vix@gmail.com>
2022-04-24 03:50:42 -0300

    created on firebase to get the infomation from user posts

--- src/hooks/useFetchDocuments.js
+++ src/hooks/useFetchDocuments.js
@@ -27,7 +27,17 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
         // query
         let q;
         if (search) {
-          q = await query(collectionRef, where("tagsArray", "array-contains", search), orderBy("createdAt", "desc"));
+          q = await query(
+            collectionRef,
+            where("tagsArray", "array-contains", search),
+            orderBy("createdAt", "desc")
+          );
+        } else if (uid) {
+          q = await query(
+            collectionRef,
+            where("uid", "==", uid),
+            orderBy("createdAt", "desc")
+          );
         } else {
           q = await query(collectionRef, orderBy("createdAt", "desc"));
         }
@@ -50,7 +60,7 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
       }
     }
     loadData();
-  }, [docCollection, documents, search, uid, cancelled]);
+  }, [docCollection, search, uid, cancelled]);
 
   useEffect(() => {
     return () => setCancelled(true);
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,37 @@
 import styles from "./Dashboard.module.css";
 
+import { Link } from "react-router-dom";
+
+// hooks
+
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+
 const Dashboard = () => {
+  const {user} = useAuthValue()
+  const uid = user.uid
+
+  // posts from user
+  const {documents: posts, loading} = useFetchDocuments("posts", null, uid);
+
   return (
     <div>
       <h1>Dashboard </h1>
+      <p>Manager your posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Not found any posts</p>
+          <Link to="/posts/create" className="btn"  >Create your post</Link>
+          </div>
+      ) : (
+        <div>
+          <p>Your posts</p>
+          </div>
+      )}
+
+      {posts && posts.map((post) => (
+        <h3>{post.title}</h3>
+      ))}
     </div>
   );
 };
